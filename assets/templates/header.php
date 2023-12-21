76c3f5d6f44fa88067f93477bd56fd9b2717ab29
Alex Moneo <alexmoneo@gmail.com>
2023-12-21 13:15:37 +0100

    Add files via upload

---
+++ assets/templates/header.php
@@ -0,0 +1,19 @@
+<header class="header">
+    <div class="grid grid-cols-2">
+        <img src="assets\images\logo.png" alt="">
+        <nav>
+            <ul class="flex flex-row">
+                <li class="mr-6"><a href="#">Products</a></li>
+                <li class="mr-6"><a href="#">Pricing</a></li>
+                <li class="mr-6"><a href="#">FAQ</a></li>
+                <li class="mr-6"><a href="#">Blog</a></li>
+                <li class="mr-6"><a href="#">Blog</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="text-right">
+        <button class="">Sign in</button>
+        <button>Sign Up</button>
+    </div>
+</header>
+
---
+++ index.php
@@ -0,0 +1,108 @@
+<?php require_once("assets/config/config.php"); ?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>   <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="es"> <![endif]-->
+<!--[if IE 7]> 		<html class="no-js lt-ie9 lt-ie8" lang="es"> <![endif]-->
+<!--[if IE 8]> 		<html class="ie8 no-js" lang="es"> <![endif]-->
+<!--[if IE 9]> 		<html class="ie9 no-js" lang="es"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="es">
+<!--<![endif]-->
+
+<head>
+     <meta charset="utf-8">
+     <meta name="author" content="Alejandro Pérez-Moneo Nieto">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <meta name="description" content="">
+     <title>Analytics — Landing Page</title>
+     <link rel="stylesheet" href="assets/css/dist/output.css" type="text/css" />
+
+</head>
+
+<body class="container max-w-screen-2xl mx-auto">
+     <?php include("assets/templates/header.php"); ?>
+     <main class="bg-header -mt-36">
+          <section class="pt-36">
+               <div>
+                    <h1>Monitor your business on real-time dashboard</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. </p>
+                    <button class="">Try for free</button>
+               </div>
+               <img src="assets\images\main-screenDoble.svg" alt="">
+          </section>
+          <section class="">
+               <h2>Main Features</h2>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet. Id vel in nam malesuada.</p>
+               <div class="">
+                    <div>
+                         <img src="assets\images\planning.svg" alt="">
+                         <h3>Monitoring 24/7</h3>
+                         <p>Lorem ipsum dolor sit amet, consectetur adipis cing elit. Elementum nisi aliquet volutpat.</p>
+                    </div>
+                    <div>
+                         <img src="assets\images\computer.svg" alt="">
+                         <h3>Widget System</h3>
+                         <p>Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar.</p>
+                    </div>
+                    <div>
+                         <img src="assets\images\speed.svg" alt="">
+                         <h3>Best Performance</h3>
+                         <p>Lorem ipsum dolor sit amet, consectetur adipis cing elit. Elementum nisi aliquet volutpat.</p>
+                    </div>
+               </div>
+          </section>
+          <section class="">
+               <div class="">
+                    <h2>Automated Reports & Widget Alerts</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet.</p>
+               </div>
+               <img src="assets\images\screen-01.svg" alt="" srcset="">
+          </section>
+          <section class="">
+               <img src="assets\images\screen-02.svg" alt="" srcset="">
+               <div class="">
+                    <h2>Fully customizable to address your needs</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet.</p>
+               </div>
+          </section>
+          <section class="">
+               <div class="">
+                    <h2>Pre-built Dashboard Templates</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet.</p>
+               </div>
+               <img src="assets\images\screen-03.svg" alt="" srcset="">
+          </section>
+          <section>
+               <h2>Pricing Plans</h2>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. </p>
+               <div class="">
+                    <div>
+                         <h3>Starter</h3>
+                         <span>up to 3 users</span>
+                         <div class="">
+                              $29<span>per month</span>
+                         </div>
+                         <button>Order</button>
+                    </div>
+                    <div >
+                         <h3>Standard</h3>
+                         <span>up to 20 users</span>
+                         <div class="">
+                              $99<span>per month</span>
+                         </div>
+                         <button class="">Order</button>
+                    </div>
+                    <div>
+                         <h3>Premium</h3>
+                         <span>up to 200 users</span>
+                         <div class="">
+                              $299<span>per month</span>
+                         </div>
+                         <button>Order</button>
+                    </div>
+               </div>
+          </section>
+     </main>
+     <?php include("assets/templates/footer.php"); ?>
+     <script src="assets/js/custom.js"></script>
+</body>
+</html>
