387ae68eb336c350d14d7a687979f2a024c6cc5b
Alex Moneo <alexmoneo@gmail.com>
2023-12-21 13:41:58 +0100

    copio practica victor

--- index.php
+++ index.php
@@ -10,94 +10,94 @@
 
 <head>
      <meta charset="utf-8">
-     <meta name="author" content="Alejandro Pérez-Moneo Nieto">
+     <meta name="author" content="Víctor Vicente Alfonso">
      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
      <meta name="description" content="">
-     <title>Analytics — Landing Page - Tailwind CSS</title>
+     <title>Analytics — Landing Page</title>
      <link rel="stylesheet" href="assets/css/dist/output.css" type="text/css" />
 
-</head>
+</head> 
 
-<body class="container max-w-screen-2xl mx-auto">
+<body  class=" font-roboto container mx-auto max-w-screen-2xl box-border bg-base">
      <?php include("assets/templates/header.php"); ?>
-     <main class="bg-header -mt-36">
-          <section class="pt-36">
-               <div>
-                    <h1>Monitor your business on real-time dashboard</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. </p>
-                    <button class="">Try for free</button>
+     <main class="mx-auto max-w-screen-xl bg-base">
+          <section class="section-1 grid grid-cols-2 h-[815px] items-center gap-[80px] text-left bg-cover bg-center bg-no-repeat mr-[-210px] mt-[-149px] pt-[164px] mx-[-210px] pl-[210px] pb-[96.43px]" style="background-image: url('assets/images/Group11.svg')">
+               <div class="h-[554px] items-center pt-[78px]">
+                    <h1 class="text-xl font-bold text-white">Monitor your business on real-time dashboard</h1>
+                    <p class="text-base1 font-normal mb-[50px] text-azul2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. </p>
+                    <button class="bg-pink-500 rounded-[30px] px-[80px] py-[10px] text-white mb-[83px]">Try for free</button>
                </div>
-               <img src="assets\images\main-screenDoble.svg" alt="">
+               <div class="bg-cover bg-left bg-no-repeat h-full w-full" style="background-image: url('assets/images/pantalla.svg')"></div>
           </section>
-          <section class="">
-               <h2>Main Features</h2>
-               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet. Id vel in nam malesuada.</p>
-               <div class="">
+          <section class="section-2 text-center pt-[38px] mb-[130px]">
+               <h2 class="text-xm font-bold text-azul1">Main Features</h2>
+               <p class="mb-[80px] mt-[30px] text-base1 font-normal text-azul2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet. Id vel in nam malesuada.</p>
+               <div class="cards grid grid-cols-3 gap-[20px]">
                     <div>
-                         <img src="assets\images\planning.svg" alt="">
-                         <h3>Monitoring 24/7</h3>
-                         <p>Lorem ipsum dolor sit amet, consectetur adipis cing elit. Elementum nisi aliquet volutpat.</p>
+                         <img class="mx-auto" src="assets\images\planning.svg" alt="">
+                         <h3 class="text-base1 font-bold leading-normal text-azul1 mb-[15px] mt-[26px]">Monitoring 24/7</h3>
+                         <p class="text-sl font-normal text-azul2">Lorem ipsum dolor sit amet, consectetur adipis cing elit. Elementum nisi aliquet volutpat.</p>
                     </div>
                     <div>
-                         <img src="assets\images\computer.svg" alt="">
-                         <h3>Widget System</h3>
-                         <p>Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar.</p>
+                         <img class="mx-auto" src="assets\images\computer.svg" alt="">
+                         <h3 class="text-base1 font-bold leading-normal text-azul1 mb-[15px] mt-[26px]">Widget System</h3>
+                         <p class="text-sl font-normal text-azul2">Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar.</p>
                     </div>
                     <div>
-                         <img src="assets\images\speed.svg" alt="">
-                         <h3>Best Performance</h3>
-                         <p>Lorem ipsum dolor sit amet, consectetur adipis cing elit. Elementum nisi aliquet volutpat.</p>
+                         <img class="mx-auto" src="assets\images\speed.svg" alt="">
+                         <h3 class="text-base1 font-bold leading-normal text-azul1 mb-[15px] mt-[26px]">Best Performance</h3>
+                         <p class="text-sl font-normal text-azul2">Lorem ipsum dolor sit amet, consectetur adipis cing elit. Elementum nisi aliquet volutpat.</p>
                     </div>
                </div>
           </section>
-          <section class="">
-               <div class="">
-                    <h2>Automated Reports & Widget Alerts</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet.</p>
+          <section class="section-3 grid grid-cols-2 items-center text-left mb-[120px]">
+               <div class="texto text-left col-span-[auto] mr-[100px]">
+                    <h2 class="text-xm font-bold leading-normal text-azul1 mb-[30px]">Automated Reports & Widget Alerts</h2>
+                    <p class="text-base1 font-normal text-azul2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet.</p>
                </div>
-               <img src="assets\images\screen-01.svg" alt="" srcset="">
+               <img class="col-span-1" src="assets\images\screen-01.svg" alt="" srcset="">
           </section>
-          <section class="">
-               <img src="assets\images\screen-02.svg" alt="" srcset="">
-               <div class="">
-                    <h2>Fully customizable to address your needs</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet.</p>
+          <section class="section-3 grid grid-cols-2 items-center text-right mb-[120px]">
+               <img class="col-span-1" src="assets\images\screen-02.svg" alt="" srcset="">
+               <div class="texto text-left col-span-[auto] ml-[100px]">
+                    <h2 class="text-xm font-bold leading-normal text-azul1 mb-[30px]">Fully customizable to address your needs</h2>
+                    <p class="text-base1 font-normal text-azul2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet.</p>
                </div>
           </section>
-          <section class="">
-               <div class="">
-                    <h2>Pre-built Dashboard Templates</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet.</p>
+          <section class="section-3 grid grid-cols-2 items-center mb-[120px]">
+               <div class="texto text-left col-span-[auto] mr-[100px]">
+                    <h2 class="text-xm font-bold leading-normal text-azul1 mb-[30px]">Pre-built Dashboard Templates</h2>
+                    <p class="text-base1 font-normal text-azul2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet.</p>
                </div>
-               <img src="assets\images\screen-03.svg" alt="" srcset="">
+               <img class="col-span-1" src="assets\images\screen-03.svg" alt="" srcset="">
           </section>
-          <section>
-               <h2>Pricing Plans</h2>
-               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. </p>
-               <div class="">
-                    <div>
-                         <h3>Starter</h3>
-                         <span>up to 3 users</span>
-                         <div class="">
-                              $29<span>per month</span>
+          <section class="section-4 text-center bg-cover bg-center bg-no-repeat mb-[-196px] mx-[-210px] px-[210px] pb-[296px] pt-[280px] text-white" style="background-image: url('assets/images/Group12.svg')">
+               <h2 class="text-xm font-bold leading-normal">Pricing Plans</h2>
+               <p class="text-base1 font-normal mt-[30px] mb-[80px] text-azul2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. </p>
+               <div class="prices grid grid-cols-3 gap-[80px] text-white">
+                    <div class="bg-card/70 rounded-[30px] pt-[50px] pb-[70px]">
+                         <h3 class="text-treinta font-bold">Starter</h3>
+                         <span class="mt-[10px] text-base1 mb-[37px] text-azul2">up to 3 users</span>
+                         <div class="precio mx-[60px] mt-[37px] pt-[28px] mb-[40px] text-60 border-t border-solid border-azul2">
+                              <span class="mb-0 text-treinta">$</span>29<span class="mb-0 block text-sl text-azul2">per month</span>
                          </div>
-                         <button>Order</button>
+                         <button class="btn px-[83px] py-[10px] bg-pink-500 rounded-[30px]  text-white">Order</button>
                     </div>
-                    <div >
-                         <h3>Standard</h3>
-                         <span>up to 20 users</span>
-                         <div class="">
-                              $99<span>per month</span>
+                    <div class="bg-white rounded-[30px] pt-[50px] pb-[70px] text-black">
+                         <h3 class="text-treinta font-bold">Standard</h3>
+                         <span class="mt-[10px] text-base1 text-azul2">up to 20 users</span>
+                         <div class="precio mx-[60px] mt-[37px] pt-[28px] mb-[40px] text-60 border-t border-solid border-azul2 items-center">
+                              <span class=" text-treinta">$</span>99<span class=" block text-sl text-azul2">per month</span>
                          </div>
-                         <button class="">Order</button>
+                         <button class="btn px-[83px] py-[10px] bg-pink-500 rounded-[30px] text-white">Order</button>
                     </div>
-                    <div>
-                         <h3>Premium</h3>
-                         <span>up to 200 users</span>
-                         <div class="">
-                              $299<span>per month</span>
+                    <div class="bg-card/70 rounded-[30px] pt-[50px] pb-[70px] ">
+                         <h3 class="text-treinta font-bold">Premium</h3>
+                         <span class="mt-[10px] text-base1 mb-[37px] text-azul2">up to 200 users</span>
+                         <div class="precio mx-[60px] mt-[37px] pt-[28px] mb-[40px] text-60 border-t border-solid border-azul2 items-center">
+                              <span class=" text-treinta">$</span>299<span class=" block text-sl text-azul2">per month</span>
                          </div>
-                         <button>Order</button>
+                         <button class="btn px-[83px] py-[10px] bg-pink-500 rounded-[30px] text-white">Order</button>
                     </div>
                </div>
           </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,29 +6,30 @@ export default {
     './assets/js/*.{html,js}',
   ],
   theme: {
-    extend: {
-      maxWidth: {
-        'screen-2xl': '1600px',
-        'screen-xl': '1180px',
-      },
-      backgroundImage: {
-        'header': "url('/assets/images/background-top.png')",
-      },
-      colors: {
-        darkblue: '#172755',
-      } 
+    fontFamily: {
+      'roboto': "Roboto"
     },
-    theme: {
-      fontFamily: {
-        'roboto': ['Roboto', 'Arial', 'sans-serif'],
+    extend: {
+      colors:{
+        'azul1': '#172755',
+        'azul2' : '#8794BA',
+        'base' : '#F8F9FF',
+        'card' : '#0F1F4B'
       },
-      container: {
-        center: true,
-      },
-    }
+      maxWidth:{
+        'screen-2xl':'1600px',
+        'screen-xl':'1180px',
+      }
+    },
+    fontSize: {
+      sm: '14px',
+      sl: '16px',
+      base1: '18px',
+      treinta: '30px',
+      xm: '40px',
+      xl: '50px',
+      60: '60px',
+    },
   },
-  plugins: [
-   
-  ],
+  plugins: [],
 }
-
